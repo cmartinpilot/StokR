ddce2f1f85cb3cd353209745f00b3094231ff6fc
Christopher Martin <christophermartin@Picard.local>
2017-05-12 23:34:06 -0400

    Working.  Add/Delete capability

--- TailsTableViewController.swift
+++ TailsTableViewController.swift
@@ -51,10 +51,15 @@ class TailsTableViewController: UITableViewController {
 
         return cell
     }
- 
+
+    
     func updateAircraft(tail: String?, row: Int){
         if tail != nil {
-            self.data[row] = tail!
+            if row >= self.data.count{
+                self.data.append(tail!)
+            }else{
+                self.data[row] = tail!
+            }
             self.tableView.reloadData()
         }
     }
@@ -68,66 +73,45 @@ class TailsTableViewController: UITableViewController {
     }
     
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            self.data.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        let cell = (sender as? TailTableViewCell)
-        let row = self.tableView.indexPath(for: cell!)?.row
-        let tail = cell?.tailLabel.text
-        
-//        let selectionIndex = self.tableView.indexPathForSelectedRow
-//        let selectedCell = self.tableView.cellForRow(at: selectionIndex!)
-//        let tail = (selectedCell as? TailTableViewCell)!.tailLabel.text
-        
-        print("prepareForSegue")
-        
-        if segue.identifier == "editTailsSegue" {
-            
-            (segue.destination as? EditTailsViewController)?.passedTail = tail
-            (segue.destination as? EditTailsViewController)?.passedRow = row
-        }
-        
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
- 
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    
+    
+            if segue.identifier == "editTailSegue" {
+    
+                let cell = (sender as? TailTableViewCell)
+                let row = self.tableView.indexPath(for: cell!)?.row
+                let tail = cell?.tailLabel.text
+    
+                (segue.destination as? EditTailsViewController)?.passedTail = tail
+                (segue.destination as? EditTailsViewController)?.passedRow = row
+            }
+    
+            if segue.identifier == "addTailSegue" {
+    
+                let editVC = (segue.destination as? EditTailsViewController)
+                editVC?.passedRow = self.data.count
+            }
 
+        }
 }
